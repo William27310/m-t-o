9d412a463c955b74f340769817318b3038f16799
William27310 <william.27@free.fr>
2025-07-03 11:33:45 +0200

    météo2

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,11 @@ fetch(url)
         document.getElementById("temp").innerHTML += `${data.list[0].main.temp}°C`
         document.getElementById("temp.max").innerHTML += `${data.list[0].main.temp_max}°C`
         document.getElementById("temp.min").innerHTML += `${data.list[0].main.temp_min}°C`
+        document.getElementById("humidity").innerHTML += `${data.list[0].main.humidity}`
         document.getElementById("ville").innerHTML += `${data.city.name}`
         document.getElementById("hour").innerHTML += `${data.list[0].dt_txt}`
+        document.getElementById("weather").innerHTML += `${data.list[0].weather[0].description}`
+        document.getElementById("wind").innerHTML += `${data.list[0].wind.deg}`
+        document.getElementById("windgust").innerHTML += `${data.list[0].wind.gust}`
+        document.getElementById("windspeed").innerHTML += `${data.list[0].wind.speed}`
     })
